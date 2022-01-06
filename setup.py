1835de018230e7376bbc928369c3a6bf46d675cb
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-01-06 15:09:19 +0400

    releeeeeease

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'lipycense',         # How you named your package folder (MyLib)
   packages = ['lipycense'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='GPL-3.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = "Helps you find out how your project's dependencies are licensed.",   # Give a short description about your library
   author = 'Aiman Al Masoud',                   # Type in your name
   author_email = 'luxlunarislabs@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/aiman-al-masoud/lipycense',   # Provide either the link to your github or to your website
   
-  download_url = 'https://github.com/aiman-al-masoud/lipycense/archive/refs/tags/v_03.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/aiman-al-masoud/lipycense/archive/refs/tags/v_04.tar.gz',    # I explain this later on
   keywords = ['license', 'python', 'packages'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
