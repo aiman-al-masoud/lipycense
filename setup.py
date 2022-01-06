3c5f89417ffde60e7f954cc01ad226bc28f17c57
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-01-06 14:23:13 +0400

    download link

--- setup.py
+++ setup.py
@@ -9,11 +9,9 @@ setup(
   author_email = 'luxlunarislabs@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/aiman-al-masoud/lipycense',   # Provide either the link to your github or to your website
   
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/aiman-al-masoud/lipycense/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['license', 'python', 'packages'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-      ],
-      
+  install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
